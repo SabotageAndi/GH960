7364ba094f04a593322b2cca38b7c398d2c9a18a
Andreas Willich <andreas.willich@techtalk.at>
2017-10-24 15:20:10 +0200

    Add project files.

---
+++ GH960/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GH960
+{
+    public class Class1
+    {
+    }
+}
